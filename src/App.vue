cb699cf8dfada98049d833a52dc40c9aecd5120a
Vagner Gon <vagnergfst@gmail.com>
2021-10-10 17:07:41 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import HelloWorld from './components/HelloWorld.vue'
+import Configs from './components/Configs.vue'
+
+</script>
+
+<template>
+  <img alt="Vue logo" src="https://simplyture.com/wp-content/uploads/2021/08/logo.svg" width="300" />
+  <HelloWorld msg="ADK config" />
+
+  <Configs />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Configs.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <div v-for="(config, i) in configFields" :key="config.id">
+            <input v-show="i + 1 < configFields.length" v-model="config.machine"/>
+            <input v-model="config.id" />
+        </div>
+        <button @click="addConfig">Add</button>
+        <hr/>
+        {{ configAsString }}
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                configFields: [{ machine: 'Machine 1', id: 0 }]
+            }
+        },
+        computed: {
+            configAsString () {
+                return this.getConfigString(this.configFields); 
+            }
+        },
+        methods: {
+            addConfig () {
+                const length = this.configFields.length;
+                this.configFields.push({ machine: `Machine ${length + 1}`, id: length })
+            },
+            
+            getConfigString(configs) {
+                if (!configs.length)
+                    return '';
+                
+                const { machine, id } = configs[0];
+
+                if (configs.length == 1)
+                    return `${id}`;
+
+                return `#{if Octopus.Machine.Id == "${machine}"}${id}#{else}${this.getConfigString(configs.slice    (1))}#{/if}`
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
