534bcbbcd1fe92fd36a935bebc136e01580c48b4
Vagner Gon <vagnergfst@gmail.com>
2021-10-10 17:43:22 -0300

    tomar no centro do cu beta do caralho

--- src/components/Configs.vue
+++ src/components/Configs.vue
@@ -2,7 +2,7 @@
     <div>
         <div v-for="(config, i) in configFields" :key="config.id">
             <input v-show="i + 1 < configFields.length" v-model="config.machine"/>
-            <input v-model="config.id" />
+            <input v-model.lazy="config.id" />
         </div>
         <button @click="addConfig">Add</button>
         <hr/>
@@ -14,7 +14,7 @@
     export default {
         data() {
             return {
-                configFields: [{ machine: 'Machine 1', id: 0 }]
+                configFields: [{ machine: 'Machine 1', id: '0' }]
             }
         },
         computed: {
@@ -25,7 +25,7 @@
         methods: {
             addConfig () {
                 const length = this.configFields.length;
-                this.configFields.push({ machine: `Machine ${length + 1}`, id: length })
+                this.configFields.push({ machine: `Machine ${length + 1}`, id: length + '' })
             },
             
             getConfigString(configs) {
@@ -37,7 +37,7 @@
                 if (configs.length == 1)
                     return `${id}`;
 
-                return `#{if Octopus.Machine.Id == "${machine}"}${id}#{else}${this.getConfigString(configs.slice    (1))}#{/if}`
+                return `#{if Octopus.Machine.Id == "${machine}"}${id}#{else}${this.getConfigString(configs.slice(1))}#{/if}`
             }
         }
     }
